0cb78b6aa6a5bc99a087ba964a47092252d4bec8
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-17 01:07:13 -0300

    fix: sqlite to postgresql

---
+++ server/prisma/migrations/20230917040604_create_database/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "transcription" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Prompt" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "template" TEXT NOT NULL,
+
+    CONSTRAINT "Prompt_pkey" PRIMARY KEY ("id")
+);
